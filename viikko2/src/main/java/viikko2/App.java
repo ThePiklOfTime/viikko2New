89c09028c90e6acb2f80cf781c79bfcd81526d03
ThePiklOfTime <nicotolvanen@gmail.com>
2026-01-07 15:55:40 +0200

    initial commit

---
+++ viikko2/src/main/java/viikko2/App.java
@@ -0,0 +1,45 @@
+package viikko2;
+
+import java.util.Scanner;
+//katsoin viikon 2 lähdekoodia selvittääkseni javan syntaxia
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Boolean exit = false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Anna auton merkki: ");
+        String brand = scanner.nextLine();
+        System.out.print("Anna auton malli: ");
+        String malli = scanner.nextLine();
+        var car = new Car(brand, malli, 0);
+
+
+        while (!exit) {
+            System.out.println("1) Näytä auton tila, \n2) Muokkaa auton merkkiä ja mallia, \n3) Kiihdytä autoa, \n4)Hidasta autoa \n0) Lopeta ohjelma");
+            
+            
+            if(scanner.hasNext()){
+                int i = Integer.parseInt(scanner.nextLine());
+            
+            switch (i) {
+                case 1:
+                    System.out.println("Mitä siili sanoo:");
+                    String sanominen = scanner.nextLine();
+                    break;
+                case 0:
+                    exit = true;
+                    break;
+                
+                default:
+                    System.out.println("Syöte oli väärä");
+                    break;
+            
+            
+                }
+        }
+    }
+    System.out.println("Kiitos ohjelman käytöstä.");
+    scanner.close();
+    }
+}
---
+++ viikko2/src/main/java/viikko2/Car.java
@@ -0,0 +1,14 @@
+package viikko2;
+
+public class Car {
+  String brand;
+  String model;
+  int speed;
+
+  public Car(String brand, String model, int speed){
+    this.brand = brand;
+    this.model = model;
+    this.speed = speed;
+    }
+    
+}

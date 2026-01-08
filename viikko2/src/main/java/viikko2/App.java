ca4a6f686f5115b24051d9794af48d68a121d2f6
ThePiklOfTime <nicotolvanen@gmail.com>
2026-01-08 11:02:46 +0200

    Removed commas

--- viikko2/src/main/java/viikko2/App.java
+++ viikko2/src/main/java/viikko2/App.java
@@ -17,7 +17,7 @@ public class App
         System.out.println();
 
         while (!exit) {
-            System.out.println("1) Näytä auton tila, \n2) Muokkaa auton merkkiä ja mallia, \n3) Kiihdytä autoa, \n4) Hidasta autoa \n0) Lopeta ohjelma");
+            System.out.println("1) Näytä auton tila \n2) Muokkaa auton merkkiä ja mallia \n3) Kiihdytä autoa \n4) Hidasta autoa \n0) Lopeta ohjelma");
             
             
             if(scanner.hasNext()){
